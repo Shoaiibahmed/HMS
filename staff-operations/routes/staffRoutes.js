5729edb21a7cc38910af0d4fc988cc7e519b0925
Shoaiibahmed <135025514+Shoaiibahmed@users.noreply.github.com>
2025-09-11 01:41:32 +0200

    Delete staff-operations directory

--- staff-operations/routes/staffRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const staffController = require('../controllers/staffController');
-const verifyToken = require('../middleware/authMiddleware');
-
-router.get('/profile', verifyToken, staffController.getProfile);
-router.put('/profile', verifyToken, staffController.updateProfile);
-
-module.exports = router;
